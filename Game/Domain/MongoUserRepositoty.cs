2174ce6314e6fe906bca1a8c6adacb5a5b0a33ab
Владислав Рева <Vladislav.Reva@urfu.me>
2025-10-21 15:51:32 +0500

    2. UserRepository

--- Game/Domain/MongoUserRepositoty.cs
+++ Game/Domain/MongoUserRepositoty.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using MongoDB.Driver;
 
 namespace Game.Domain
@@ -11,35 +12,51 @@ namespace Game.Domain
         public MongoUserRepository(IMongoDatabase database)
         {
             userCollection = database.GetCollection<UserEntity>(CollectionName);
+
+            var keys = Builders<UserEntity>.IndexKeys.Ascending(x => x.Login);
+            var indexOptions = new CreateIndexOptions { Unique = true };
+            userCollection.Indexes.CreateOne(new CreateIndexModel<UserEntity>(keys, indexOptions));
         }
 
         public UserEntity Insert(UserEntity user)
         {
-            //TODO: Ищи в документации InsertXXX.
-            throw new NotImplementedException();
+            userCollection.InsertOne(user);
+            return user;
         }
 
         public UserEntity FindById(Guid id)
         {
-            //TODO: Ищи в документации FindXXX
-            throw new NotImplementedException();
+            return userCollection.Find(u => u.Id == id).FirstOrDefault();
         }
 
         public UserEntity GetOrCreateByLogin(string login)
         {
-            //TODO: Это Find или Insert
-            throw new NotImplementedException();
+            var filter = Builders<UserEntity>.Filter.Eq(x => x.Login, login);
+            var updateDefinition = Builders<UserEntity>.Update
+                .SetOnInsert(x => x.Id, Guid.NewGuid())
+                .SetOnInsert(x => x.Login, login)
+                .SetOnInsert(x => x.FirstName, string.Empty)
+                .SetOnInsert(x => x.LastName, string.Empty)
+                .SetOnInsert(x => x.CurrentGameId, null)
+                .SetOnInsert(x => x.GamesPlayed, 0);
+
+            var findOneAndUpdateOptions = new FindOneAndUpdateOptions<UserEntity, UserEntity>
+            {
+                IsUpsert = true,
+                ReturnDocument = ReturnDocument.After
+            };
+            
+            return userCollection.FindOneAndUpdate(filter, updateDefinition, findOneAndUpdateOptions);
         }
 
         public void Update(UserEntity user)
         {
-            //TODO: Ищи в документации ReplaceXXX
-            throw new NotImplementedException();
+            userCollection.ReplaceOne(u => u.Id == user.Id, user);
         }
 
         public void Delete(Guid id)
         {
-            throw new NotImplementedException();
+            userCollection.DeleteOne(u => u.Id == id);
         }
 
         // Для вывода списка всех пользователей (упорядоченных по логину)
@@ -47,7 +64,15 @@ namespace Game.Domain
         public PageList<UserEntity> GetPage(int pageNumber, int pageSize)
         {
             //TODO: Тебе понадобятся SortBy, Skip и Limit
-            throw new NotImplementedException();
+            var totalCount = userCollection.CountDocuments(x => true);
+            var pageEntities = userCollection
+                .Find(x => true)
+                .Sort(Builders<UserEntity>.Sort.Ascending(u => u.Login))
+                .Skip(pageSize * (pageNumber - 1))
+                .Limit(pageSize)
+                .ToList();
+            
+            return new PageList<UserEntity>(pageEntities, totalCount, pageNumber, pageSize);
         }
 
         // Не нужно реализовывать этот метод
